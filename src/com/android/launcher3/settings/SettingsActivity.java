d4a3084bafea12adc6d5b536a8b71a02405f913d
Varun Date <date.varun123@gmail.com>
2020-11-10 18:36:05 +1100

    Revert "Launcher3: categorize settings"
    This reverts commit fba0af5f2f58ea7218e8dd9dcdaef6f60ad07c92.
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I7037ef248759e6c75c071f6301b6634def5bc905

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -36,7 +36,6 @@ import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceFragment.OnPreferenceStartFragmentCallback;
 import androidx.preference.PreferenceFragment.OnPreferenceStartScreenCallback;
@@ -174,19 +173,9 @@ public class SettingsActivity extends Activity
         private void updatePreferences() {
             PreferenceScreen screen = getPreferenceScreen();
             for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
-                final Preference preference = screen.getPreference(i);
-                if (preference instanceof PreferenceCategory) {
-                    final PreferenceCategory category = (PreferenceCategory) preference;
-                    for (int j = category.getPreferenceCount() - 1; j >= 0; j--) {
-                        final Preference p = category.getPreference(j);
-                        if (!initPreference(p)) {
-                            category.removePreference(p);
-                        }
-                    }
-                } else {
-                    if (!initPreference(preference)) {
-                        screen.removePreference(preference);
-                    }
+                Preference preference = screen.getPreference(i);
+                if (!initPreference(preference)) {
+                    screen.removePreference(preference);
                 }
             }
         }
