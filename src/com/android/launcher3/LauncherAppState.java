BlissRoms/platform_packages_apps_Launcher3

Launcher3: fix imports
Change-Id: I0187ec82a0df2100949cfd7682376c10025f42f3

src/com/android/launcher3/LauncherAppState.java
@@ -40,7 +40,7 @@ import com.android.launcher3.util.Preconditions;
 import com.android.launcher3.util.SecureSettingsObserver;
 import com.android.launcher3.widget.custom.CustomWidgetManager;
 
-import com.android.internal.util.aicp.PackageUtils;
+import com.android.internal.util.bliss.BlissUtils;
 
 public class LauncherAppState {
 
@@ -84,8 +84,8 @@ public class LauncherAppState {
         Preconditions.assertUIThread();
         mContext = context;
 
-        setSearchAppAvailable(PackageUtils.isPackageInstalled(context, Utilities.PACKAGE_NAME));
-        setCalendarAppAvailable(PackageUtils.isPackageInstalled(context, "com.google.android.calendar"));
+        setSearchAppAvailable(BlissUtils.isPackageInstalled(context, Utilities.PACKAGE_NAME));
+        setCalendarAppAvailable(BlissUtils.isPackageInstalled(context, "com.google.android.calendar"));
         mInvariantDeviceProfile = InvariantDeviceProfile.INSTANCE.get(mContext);
         mIconCache = new IconCache(mContext, mInvariantDeviceProfile);
         mWidgetCache = new WidgetPreviewLoader(mContext, mIconCache);

ResurrectionRemix/packages_apps_Trebuchet

src/com/android/launcher3/LauncherAppState.java
@@ -39,7 +39,7 @@ import com.android.launcher3.util.Preconditions;
 import com.android.launcher3.util.SecureSettingsObserver;
 import com.android.launcher3.widget.custom.CustomWidgetManager;
 
-import com.android.internal.util.crdroid.Utils;
+import com.android.internal.util.rr.Utils;
 
 public class LauncherAppState {
 
@@ -84,7 +84,6 @@ public class LauncherAppState {
 
         setSearchAppAvailable(Utils.isPackageInstalled(context, Utilities.SEARCH_PACKAGE));
         setCalendarAppAvailable(Utils.isPackageInstalled(context, "com.google.android.calendar"));
-
         mInvariantDeviceProfile = InvariantDeviceProfile.INSTANCE.get(mContext);
         mIconCache = new IconCache(mContext, mInvariantDeviceProfile);
         mWidgetCache = new WidgetPreviewLoader(mContext, mIconCache);